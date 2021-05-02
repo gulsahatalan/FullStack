f69d7fd479c49c92ceecefc033b0206e15cdf5b2
gulsahatalan <atalangulsah@gmail.com>
2021-05-02 13:30:46 +0200

    add week7

--- week7/React HTTP POST Request(Fetch)/form/src/components/form.js
+++ week7/React HTTP POST Request(Fetch)/form/src/components/form.js
@@ -1,35 +1,34 @@
-import { React, useState } from "react";
+import  React, { useReducer, useState } from "react";
 
+const formReducer = (state, event) => {
+  return {
+    ...state,
+    [event.name]: event.value
+  }
+ }
 export default function Form() {
+  const [formData, setFormData] = useReducer(formReducer, {});
+
   const handleSubmit = (event) => {
     event.preventDefault();
     createPerson(event);
-    console.log(
-      event.target.firstName.value,
-      event.target.lastName.value,
-      event.target.gender.value,
-      event.target.birthday.value,
-      event.target.email.value,
-      event.target.password.value,
-      event.target.about.value
-    );
+  
   };
+  
+  const handleChange = event => {
+    setFormData({
+      name: event.target.name,
+      value: event.target.value,
+    });
+  }
   // post metodu ile person nesnesini yolluyoruz
   const createPerson = async (event) => {
     let response = await fetch("http://174.138.103.233/api/employees", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: await JSON.stringify({
-        firstName: event.target.firstName.value,
-        lastName: event.target.lastName.value,
-        gender: event.target.gender.value,
-        birthday: event.target.birthday.value,
-        email: event.target.email.value,
-        password: event.target.password.value,
-        about: event.target.about.value,
-      }),
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(formData)
+      
+      
     });
     let data = await response.json();
     return data;
@@ -41,36 +40,36 @@ export default function Form() {
         <fieldset>
           <label>
             <p>First Name</p>
-            <input required name="firstName" type="text"></input>
+            <input required name="firstName" type="text"  onChange={handleChange}></input>
           </label>
           <label>
             <p>Last Name </p>
-            <input required name="lastName" type="text"></input>
+            <input required name="lastName" type="text"  onChange={handleChange}></input>
           </label>
           <label>
             <p>Gender </p>
             <span>
-              <input type="radio" name="gender" value="male"></input>Male
+              <input type="radio" name="gender" value="male"  onChange={handleChange}></input>Male
             </span>
             <span>
-              <input type="radio" name="gender" value="female"></input>Female
+              <input type="radio" name="gender" value="female"  onChange={handleChange}></input>Female
             </span>
           </label>
           <label>
             <p>Birtday </p>
-            <input type="date" name="birthday"></input>
+            <input type="date" name="birthday"  onChange={handleChange}></input>
           </label>
           <label>
             <p>E-Mail </p>
-            <input type="email" name="email"></input>
+            <input type="email" name="email"  onChange={handleChange}></input>
           </label>
           <label>
             <p>Password</p>
-            <input type="password" name="password"></input>
+            <input type="password" name="password"  onChange={handleChange}></input>
           </label>
           <label>
             <p>About Me </p>
-            <input required name="about"></input>
+            <input required name="about"  onChange={handleChange}></input>
           </label>
         </fieldset>
         <button type="submit">Submit</button>
